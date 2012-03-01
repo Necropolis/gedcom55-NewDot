9cbb91d839ef8d1045dc87ddaae3289a1acf4f91
Chris Miller <nserror@me.com>
2012-03-01 14:18:16 -0700

    More assertions!

--- FSGEDCOMIndividual+NewDot.m
+++ FSGEDCOMIndividual+NewDot.m
@@ -17,15 +17,17 @@
 
 - (NSDictionary *)nfs_assertionsDescribingIndividual
 {
-   NSMutableDictionary * assertions = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                                       [NSMutableArray array], NDFamilyTreeAssertionType.characteristics,
-                                       [NSMutableArray array], NDFamilyTreeAssertionType.citations,
-                                       [NSMutableArray array], NDFamilyTreeAssertionType.events,
-                                       [NSMutableArray array], NDFamilyTreeAssertionType.exists,
-                                       [NSMutableArray array], NDFamilyTreeAssertionType.genders,
-                                       [NSMutableArray array], NDFamilyTreeAssertionType.names,
-                                       [NSMutableArray array], NDFamilyTreeAssertionType.ordinances,
-                                       [NSMutableArray array], NDFamilyTreeAssertionType.notes, nil];
+    NSLog(@"%@", [self descriptionWithLocale:nil indent:0]);
+    
+    NSMutableDictionary * assertions = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                        [NSMutableArray array], NDFamilyTreeAssertionType.characteristics,
+                                        [NSMutableArray array], NDFamilyTreeAssertionType.citations,
+                                        [NSMutableArray array], NDFamilyTreeAssertionType.events,
+                                        [NSMutableArray array], NDFamilyTreeAssertionType.exists,
+                                        [NSMutableArray array], NDFamilyTreeAssertionType.genders,
+                                        [NSMutableArray array], NDFamilyTreeAssertionType.names,
+                                        [NSMutableArray array], NDFamilyTreeAssertionType.ordinances,
+                                        [NSMutableArray array], NDFamilyTreeAssertionType.notes, nil];
     
     /*
      ================================================ what's in a name?
@@ -39,9 +41,7 @@
         },
      ]
      */
-    FSGEDCOMStructure * name = [[self.elements objectForKey:@"NAME"] firstObject];
-    if (name) {
-    
+    [[self.elements objectForKey:@"NAME"] enumerateObjectsUsingBlock:^(FSGEDCOMStructure * name, NSUInteger idx, BOOL *stop) {
         NSError * error;
         NSRegularExpression * removeSlashes = [NSRegularExpression regularExpressionWithPattern:@"(?:\\/)([\\w^\\/]+)(?:\\/)" options:0 error:&error];
         if (error)
@@ -50,9 +50,71 @@
         [[assertions objectForKey:NDFamilyTreeAssertionType.names] addObject:
          [NSDictionary dictionaryWithObject:[NSArray arrayWithObject:[NSDictionary dictionaryWithObject:[removeSlashes stringByReplacingMatchesInString:[name value] options:0 range:NSMakeRange(0, [[name value] length]) withTemplate:@"$1"] forKey:@"fullText"]]
                                      forKey:@"forms"]];
+    }];
+    /*
+     ================================================ events are important, mkay?
+     "events" : [
+        {
+            "value" : {
+                "type" : (Adoption, Adult Christening, Baptism, Confirmation, Bar Mitzvah, Bas Mitzvah, Birth, Blessing, Burial, Christening, Cremation, Death, Graduation, Immigration, Military Service, Mission, Move, Naturalization, Probate, Retirement, Will, Census, Circumcision, Emigration, Excommunication, First Communion, First Known Child, Funeral, Hospitalization, Illness, Naming, Miscarriage, Ordination, Other),
+                "place" : <place stuff>,
+                "date" : <date stuff>
+            }
+        }
+     ]
+     */
+    [[self.elements objectForKey:@"BIRT"] enumerateObjectsUsingBlock:^(FSGEDCOMStructure * birth, NSUInteger idx, BOOL *stop) {
+        NSMutableDictionary * birthEvent = [[NSMutableDictionary alloc] initWithCapacity:3];
+        [birthEvent setObject:@"Birth" forKey:@"type"];
         
-    }
-    
+        FSGEDCOMStructure * dateOfBirth = [[birth.elements objectForKey:@"DATE"] firstObject];
+        if (dateOfBirth) {
+            [birthEvent setObject:[NSDictionary dictionaryWithObject:[dateOfBirth value] forKey:@"original"] forKey:@"date"];
+        }
+        FSGEDCOMStructure * placeOfBirth = [[birth.elements objectForKey:@"PLAC"] firstObject];
+        if (placeOfBirth) {
+            [birthEvent setObject:[NSDictionary dictionaryWithObject:[placeOfBirth value] forKey:@"original"] forKey:@"place"];
+        }
+        
+        [[assertions objectForKey:NDFamilyTreeAssertionType.events] addObject:birthEvent];
+    }];
+    [[self.elements objectForKey:@"DEAT"] enumerateObjectsUsingBlock:^(FSGEDCOMStructure * death, NSUInteger idx, BOOL *stop) {
+        NSMutableDictionary * deathEvent = [[NSMutableDictionary alloc] initWithCapacity:3];
+        [deathEvent setObject:@"Death" forKey:@"type"];
+        
+        FSGEDCOMStructure * dateOfDeath = [[death.elements objectForKey:@"DATE"] firstObject];
+        if (dateOfDeath) {
+            [deathEvent setObject:[NSDictionary dictionaryWithObject:[dateOfDeath value] forKey:@"original"] forKey:@"date"];
+        }
+        FSGEDCOMStructure * placeOfDeath = [[death.elements objectForKey:@"PLAC"] firstObject];
+        if (placeOfDeath) {
+            [deathEvent setObject:[NSDictionary dictionaryWithObject:[placeOfDeath value] forKey:@"original"] forKey:@"place"];
+        }
+        
+        [[assertions objectForKey:NDFamilyTreeAssertionType.events] addObject:deathEvent];
+    }];
+    /*
+     ================================================ gender has meaning
+     "genders" : [
+        {
+            "value" : {
+                "type" : (Male, Female, Unknown)
+            }
+        }
+     ]
+     */
+    [[self.elements objectForKey:@"SEX"] enumerateObjectsUsingBlock:^(FSGEDCOMStructure * gender, NSUInteger idx, BOOL *stop) {
+        NSMutableDictionary * genderAssertion = [NSMutableDictionary dictionary];
+        NSString * val = [gender value];
+        if ([val compare:@"male" options:NSCaseInsensitiveSearch]==NSOrderedSame) {
+            [genderAssertion setObject:[NSDictionary dictionaryWithObject:@"Male" forKey:@"type"] forKey:@"value"];
+        } else if ([val compare:@"female" options:NSCaseInsensitiveSearch]==NSOrderedSame) {
+            [genderAssertion setObject:[NSDictionary dictionaryWithObject:@"Female" forKey:@"type"] forKey:@"value"];
+        } else { // unknown
+            [genderAssertion setObject:[NSDictionary dictionaryWithObject:@"Unknown" forKey:@"type"] forKey:@"value"];
+        }
+        [[assertions objectForKey:NDFamilyTreeAssertionType.genders] addObject:genderAssertion];
+    }];
     
 
     // remove empty ones
